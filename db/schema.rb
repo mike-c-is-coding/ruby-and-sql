6f92d992e0ce81356199c78beaaafdfaaca3c9a6
Michael Carter <mike.carter@kellogg.northwestern.edu>
2025-02-04 20:44:53 +0000

    completed lab 1

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_02_04_201703) do
+ActiveRecord::Schema[7.1].define(version: 2025_02_04_202411) do
+  create_table "activities", force: :cascade do |t|
+    t.string "occurred_at"
+    t.string "notes"
+    t.integer "salesperson_id"
+    t.integer "contact_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "companies", force: :cascade do |t|
     t.string "name"
     t.string "city"
